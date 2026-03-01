af33609894fe1cd6280719db9d4bd121ff2e0adc
Cloue M <senp41.mac@gmail.com>
2026-03-01 12:27:23 -0800

    Fixed errors and made clean codes

--- functions.php
+++ functions.php
@@ -1,20 +1,17 @@
 <?php
 function school_site_enqueue_assets() {
-    
-    if ( is_front_page() ) {
-        // 1. lightGallery CSS (CDN)
-        wp_enqueue_style( 'lightgallery-css', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/css/lightgallery-bundle.min.css' );
 
-        // 2. lightGallery JS (CDN)
+    if ( is_front_page() ) {
+        wp_enqueue_style( 'lightgallery-css', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/css/lightgallery-bundle.min.css', array(), '2.7.2' );
+
         wp_enqueue_script( 'lightgallery-js', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/lightgallery.min.js', array(), '2.7.2', true );
 
-        // 3. Settings File
-        wp_enqueue_script( 
-            'school-gallery-init', 
-            get_template_directory_uri() . '/js/lightgallery-init.js', // Match your screenshot
-            array('lightgallery-js'), 
-            '1.0.0', 
-            true 
+        wp_enqueue_script(
+            'school-gallery-init',
+            get_template_directory_uri() . '/assets/js/lightgallery-init.js',
+            array( 'lightgallery-js' ),
+            '1.0.0',
+            true
         );
     }
 }
