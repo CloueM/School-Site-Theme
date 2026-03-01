941caa1545e4cf82788d88ac06ccc0260f440af6
Cloue M <senp41.mac@gmail.com>
2026-03-01 14:52:24 -0800

    Fix custom blocks

--- school-site-blocks/build/company-address/index.asset.php
+++ school-site-blocks/build/company-address/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '2ee5166911360fee460e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '32f27bff3b4cb0fb4d42');
